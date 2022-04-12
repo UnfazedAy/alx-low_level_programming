c06e4ca45f1ed161f3931e06ad415d3a1a3d56e6
UnfazedAy <ayomidesoniyi@gmail.com>
2022-04-12 14:16:06 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include "6-abs.c"
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints natural numbers up to the number 98
+ * Followed by a newline
+ *
+ * @n: print from this number
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
