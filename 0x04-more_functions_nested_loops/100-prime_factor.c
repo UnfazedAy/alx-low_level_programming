90935d6016371f1d2007e927c94ae259fa3c1646
UnfazedAy <ayomidesoniyi@gmail.com>
2022-04-14 11:17:39 -0700

    The problem of distinguishing prime numbers from composite numbers and of resolving the latter into their prime factors is known to be one of the most important and useful in arithmetic

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,28 +1,31 @@
-#include "main.h"
+#include <stdio.h>
+
 /**
-*print_triangle - prints a triangle
-*@size:size parameter of triangle
-*Return:returns nothing
+*main- prints the largest prime factor
+*of a number
+*
+*Return: returns 0
 */
-void print_triangle(int size)
-{
-int inc1, inc2;
 
-if (size > 0)
+int main(void)
 {
-for (inc1 = 1; inc1 <= size; inc1++)
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
 {
-for ((inc2 = size - inc1); inc2 > 0; inc2--)
-_putchar(' ');
-
-for (inc2 = 0; inc2 < inc1; inc2++)
-_putchar('#');
-
-if (inc1 == size)
+if (number % inc == 0)
+{
+number /= 2;
 continue;
+}
 
-_putchar('\n');
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
+
 }
 }
-_putchar('\n');
+printf("%ld\n", number);
+return (0);
 }
