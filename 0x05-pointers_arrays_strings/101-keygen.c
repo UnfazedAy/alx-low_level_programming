b30eac25964ac1a129971ec823417d31d605819f
UnfazedAy <ayomidesoniyi@gmail.com>
2022-04-20 15:37:55 -0700

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,36 +1,21 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-/**
- * _atoi - Convert strings to integers
- * @s: character
- * Return: 0 (success)
- */
-
-int _atoi(char *s)
+int main(void)
 {
-int i;
-int z, p;
+int rand_passwd;
+int count;
+int total;
 
-z = 0;
-p = -1;
-for (i = 0; s[i] != '\0'; i++)
+srand(time(0));
+for (count = 0, total = 2772; total > 122; count++)
 {
-if (s[i] == '-')
-p *= -1;
-
-if (s[i] > 47 && s[i] < 58)
-{
-if (z < 0)
-z = (z * 10) - (s[i] - '0');
-else
-z = (s[i] - '0') * -1;
-
-if (s[i + 1] < 48 || s[i + 1] > 57)
-break;
+rand_passwd = (rand() % 125) + 1;
+printf("%c", rand_passwd);
+total -= rand_passwd;
 }
-}
-if (p < 0)
-z *= -1;
+printf("%c", total);
 
-return (z);
+return (0);
 }
