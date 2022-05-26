00f1eefebbf4ca28f7bacfba668a752dcdec912a
UnfazedAy <ayomidesoniyi@gmail.com>
2022-05-26 05:34:19 -0700

    A function that frees a listint_t list. This function can free lists with a loop, You should go though the list only once. Returns: the size of the list that was free’d, The function sets the head to NULL

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -66,4 +66,23 @@ size_t print_listint_safe(const listint_t *head)
 
 	if (nodes == 0)
 	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
 
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
+}
