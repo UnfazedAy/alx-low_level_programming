03e923b097540fddfcab69585e35a9b9398bf712
UnfazedAy <ayomidesoniyi@gmail.com>
2022-05-26 04:52:44 -0700

    My lists.h file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,24 +2,24 @@
 #define LISTS_H
 
 /*
- *  * Desc: Header file containing prototypes and definitions for all functions
- *   *       and types written in the 0x13-more_singly_linked_lists directory.
- *    */
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x13-more_singly_linked_lists directory.
+ */
 
 #include <stdlib.h>
 
 /**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * for alx student project
- *        */
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx student project
+ */
 typedef struct listint_s
 {
-		int n;
-			struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
